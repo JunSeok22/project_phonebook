e866ffe7ab3ad6e909ad23777c0a2f2b3f058ff8
JunSeok22 <jslee3518@nate.com>
2015-06-10 22:40:55 +0900

    수정함

--- src/sample/sample.java
+++ src/sample/sample.java
@@ -3,7 +3,7 @@ package sample;
 public class sample {
 
 	public static void main(String[] args) {
-		System.out.println("github 연동법 연습");
+		System.out.println("github 연동법 연습 ?? give to ecilpse from git");
 
 	}
 
